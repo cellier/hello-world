6f135d5cbc9466219938d21d6636752fe95a44f6
cellier <shengbixing@gmail.com>
2019-03-06 22:21:15 +0800

    fix sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import "../styles/global.scss"
 
 export default () => (
   <div style={{ color: "purple" }}>
     <Link to="/about">About</Link>
-    <h1>Hello Gatsby!</h1>
+    <h1>Hello Gatsby2!</h1>
     <p>what a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
